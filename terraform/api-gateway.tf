d61d040b87fa40126378e2b33d20d839fe8cc40b
GabrielFrahm <gabriel_frahm@hotmail.com>
2025-04-22 23:58:12 -0300

    feat

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,33 +1,58 @@
-
-resource "aws_apigatewayv2_api" "http_api" {
-  name          = "${var.prefix}-http-api"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "api" {
+  name = "${var.prefix}-rest-api"
 }
 
-resource "aws_apigatewayv2_vpc_link" "ecs_link" {
-  name               = "${var.prefix}-vpc-link"
-  subnet_ids         =  module.new-vpc.subnet_ids
-  security_group_ids = [module.new-vpc.security_group_id]
+resource "aws_api_gateway_resource" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
 }
 
-resource "aws_apigatewayv2_integration" "ecs_integration" {
-  api_id                 = aws_apigatewayv2_api.http_api.id
-  integration_type       = "HTTP_PROXY"
-  connection_type        = "VPC_LINK"
-  connection_id          = aws_apigatewayv2_vpc_link.ecs_link.id
-  integration_method     = "ANY"
-  integration_uri        = aws_lb_listener.nlb_listener.arn
-  payload_format_version = "1.0"
+resource "aws_api_gateway_method" "proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
 }
 
-resource "aws_apigatewayv2_route" "root_proxy" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "ANY /{proxy+}"
-  target    = "integrations/${aws_apigatewayv2_integration.ecs_integration.id}"
+resource "aws_api_gateway_integration" "proxy_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy_method.http_method
+  integration_http_method = "ANY"
+  type                    = "HTTP"
+  uri                     = "http://${aws_lb.nlb.dns_name}:3333/{proxy}"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
 }
 
-resource "aws_apigatewayv2_stage" "dev" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "dev"
-  auto_deploy = true
+resource "aws_api_gateway_method_settings" "all" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_stage.stage.stage_name
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+    data_trace_enabled = true
+  }
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.proxy_integration]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "ignored"
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "prod"
+}
+
+output "api_gateway_url" {
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}"
 }
