0b7d1e6485653c0e1987ae3253c8e5d71c0e8e61
GabrielFrahm <gabriel_frahm@hotmail.com>
2025-05-17 08:44:55 -0300

    cognito

--- api/modules/user/usecases/create-user.usecase.ts
+++ api/modules/user/usecases/create-user.usecase.ts
@@ -1,3 +1,7 @@
+import {
+  CognitoIdentityProviderClient,
+  AdminInitiateAuthCommand,
+} from '@aws-sdk/client-cognito-identity-provider';
 import { BaseUseCase } from '@shared/interfaces/usecase.interfaces';
 import { CreateUserDto, OutputUserDto } from '../dtos/user.dto';
 import { Either, left, right } from '@shared/either';
@@ -9,14 +13,36 @@ import { UserEntity } from '../entities/user.entity';
 export class CreateUserUseCase
   implements BaseUseCase<CreateUserDto, Either<Error, OutputUserDto>>
 {
+  private readonly cognito = new CognitoIdentityProviderClient({
+    region: process.env.AWS_REGION || 'us-east-1',
+  });
+
   constructor(
     @Inject('userRepository')
     private readonly userRepository: UserRepositoryInterface,
   ) {}
+
   async execute(input: CreateUserDto): Promise<Either<Error, OutputUserDto>> {
     const user = await this.userRepository.create(UserEntity.CreateNew(input));
     if (user.isLeft()) {
       return left(user.value);
+    }
+
+    const email = input.email;
+
+    try {
+      await this.cognito.send(
+        new AdminInitiateAuthCommand({
+          UserPoolId: process.env.COGNITO_USER_POOL_ID,
+          ClientId: process.env.COGNITO_CLIENT_ID,
+          AuthFlow: 'CUSTOM_AUTH',
+          AuthParameters: {
+            USERNAME: email,
+          },
+        }),
+      );
+    } catch (err) {
+      return left(new Error(`Erro ao iniciar autenticação no Cognito: ${err}`));
     }
 
     return right({
